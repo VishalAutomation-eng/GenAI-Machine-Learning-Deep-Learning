a9daf426ab27d2d06f112707f3f515dedaf5c5e3
vkpande <vkpande@expediens.com>
2026-02-03 13:07:33 +0530

    remove day10

--- Day2-Netflix_Content_Analysis/src/feature_engineering.py
+++
@@ -1,3 +0,0 @@
-def add_content_age(df):
-    df['age_on_netflix'] = df['year_added'] - df['release_year']
-    return df[df['age_on_netflix'] >= 0]
--- Day5-Customaer_segmentation_with_Clustering/src/data_loader.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def load_data(path: str) -> pd.DataFrame:
-    df = pd.read_csv(path)
-    print("Dataset loaded successfully")
-    print("Shape:", df.shape)
-    return df
